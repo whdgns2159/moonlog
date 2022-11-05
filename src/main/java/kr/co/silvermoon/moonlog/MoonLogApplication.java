b05b5fb16edcf8d7b2258ffc2dd9cbb98936844f
moon <whdgns2159@naver.com>
2022-11-05 15:59:02 +0900

    init

---
+++ src/main/java/kr/co/silvermoon/moonlog/MoonLogApplication.java
@@ -0,0 +1,13 @@
+package kr.co.silvermoon.moonlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonLogApplication.class, args);
+	}
+
+}
