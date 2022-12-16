9d496d95cb0fdd08048c48ec6eab029aee530c1e
MOON <lune6669@ant-soft.co.kr>
2022-12-16 16:02:41 +0900

    프론트에서 백엔드 서버로 프록시 설정

--- src/front/next.config.js
+++ src/front/next.config.js
@@ -1,6 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
+  compiler : {
+    styleComponents : true
+  },
+  async rewrites(){
+    if(process.env.NODE_ENV !== 'production'){
+      return [
+        {
+          destination: 'http://localhost:8090/:path',
+          source: '/api/:path',
+        }
+      ]
+    }else{
+      return []
+    }
+  },
+  reactStrictMode : true
 }
-
-module.exports = nextConfig
