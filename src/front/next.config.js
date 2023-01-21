47ee3cecc6c7914b95efb5e1afa171b4d1f0a8cc
moon <whdgns2159@naver.com>
2023-01-21 19:31:45 +0900

    기본 설정

--- src/front/next.config.js
+++ src/front/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = (phase, {defaultConfig}) =>{
+  const rewrites = () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8080/:path*"
+      },
+    ];
+  }
+
+  return {rewrites}
+}
