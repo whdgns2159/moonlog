a8d9fe7ebbaf7764ba52c7de923330634bc3609e
MOON <lune6669@ant-soft.co.kr>
2023-02-27 09:05:44 +0900

    주말에 작성한것이랑 병합

--- src/front/app/login/page.tsx
+++ src/front/app/login/page.tsx
@@ -1,14 +1,19 @@
 'use client'
 import styled from "styled-components";
+import {FieldErrors, useForm} from "react-hook-form";
 import {MIXINS} from "@/styles/theme";
-import React, {FormEvent} from "react";
+import React, {useCallback} from "react";
+import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import {getCsrfToken, signIn} from "next-auth/react"
 import { yupResolver } from '@hookform/resolvers/yup';
-import {useForm} from "react-hook-form";
 import * as Yup from 'yup';
-import {signIn} from "next-auth/react";
 
+type HookFormType = {
+    username : String,
+    password : String
+}
 
-function Login() {
+function Login({ csrfToken }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     //register를 사용하면 validation으로 값을 입력못하게 막을 수 있다.
     const validationSchema = Yup.object().shape({
         // title: Yup.string()
@@ -32,21 +37,30 @@ function Login() {
         //     .oneOf([true], 'Accept Ts & Cs is required')
     });
     const formOptions = { resolver: yupResolver(validationSchema) };
-    const {handleSubmit} = useForm(formOptions);
-    function onSubmit (){
-        signIn("credentials", {redirect: false , callbackUrl : "http://localhost:3000/article/"})
+    const {handleSubmit} = useForm<HookFormType>(formOptions);
+    function onSubmit (body: HookFormType ){
+        signIn("credentials", {redirect: false , callbackUrl : "http://localhost:3000/article/", ...body})
         return false;
     }
 
     return (
-        <Form onSubmit={(e) => {handleSubmit(onSubmit)}}>
+        <Form onSubmit={body => {handleSubmit(onSubmit)}}>
+            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
             <SpanTitle>아이디</SpanTitle>
             <InputId name={"username"} id={"username"} type={"text"}/>
             <SpanTitle>비밀번호</SpanTitle>
             <InputPwd name={"password"} id={"password"} type={"password"}/>
-            <ButtonSubmit type={"submit"}>로그인</ButtonSubmit>
+            <InputButtonSubmit type={"submit"}>로그인</InputButtonSubmit>
         </Form>
     )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    return {
+        props: {
+            csrfToken: await getCsrfToken(context),
+        },
+    }
 }
 
 const Form = styled.form`
@@ -61,7 +75,7 @@ const InputPwd = styled.input`
     margin: 4px 0;
     font-size: 1.2rem;
 `
-const ButtonSubmit = styled.button`
+const InputButtonSubmit = styled.button`
     width: 100%;
 `
 
--- src/main/java/kr/co/silvermoon/moonlog/business/domain/User.java
+++ src/main/java/kr/co/silvermoon/moonlog/business/domain/User.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "USER")
+@Table(name = "BLOG_USER")
 @Getter
 public class User {
 
