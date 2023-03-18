a376660bf0bb9131da7eeb09bfc0cf5ed5d02a6a
moon <whdgns2159@naver.com>
2023-03-18 21:52:29 +0900

    회원가입요청 페이지 작업중

--- src/front/app/user/signup/page.tsx
+++ src/front/app/user/signup/page.tsx
@@ -1,12 +1,21 @@
+import {router} from "next/client";
+
 function SignUp(){
     async function signUp(){
-        const rst = await fetch("http://localhost:3001/auth/signin");
-        return rst.json();
+        const rst = await fetch("http://localhost:3001/auth/signin")
+            .then((res) =>{
+                alert("회원가입 완료");
+
+            }).catch((err) => {
+
+            });
+        return rst;
     }
     return (
         <form onSubmit={signUp}>
-
-
+            <input name={"username"} type={"text"}/>
+            <input name={"password"} type={"password"}/>
+            <button type={"submit"}>회원가입</button>
         </form>
     );
 }
